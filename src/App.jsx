a2b624d17a2f877765ddc52cb6b3f06865a52f3d
zokof <vip.salm2@gmail.com>
2025-04-28 13:45:32 +0300

    react router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import { useState } from 'react'
 
-import AddCommentForm from './components/AddCommentForm'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import PostList from './components/PostList'
+import Post from './components/Post'
 
 function App() {
 
   return (
-    <>
-      <AddCommentForm />
-    </>
+    <div>
+        APP
+      <Routes>
+        <Route path="/" element={<PostList />} />
+        <Route path="/posts/:id" element = {<Post />} />
+      </Routes>
+    </div>
   )
 }
 
