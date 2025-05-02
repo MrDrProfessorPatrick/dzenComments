56e9bc964c6e8ad015bf7cd682f0e65784927334
zokof <vip.salm2@gmail.com>
2025-05-02 17:33:50 +0300

    websocket server with message and close listeners added to server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import { WebSocketServer } from 'ws';
 // import cookieParser from 'cookie-parser';
+import { createServer } from 'http';
 import dotenv from "dotenv"
 
 import commentsRouter from './comments/commentsRouter.js';
@@ -19,5 +21,35 @@ app.use('/api', cors({
     commentsRouter
 );
 
+const server = createServer(app);
+
+const wss = new WebSocketServer({ server, path: '/ws' });
+
+const clients = new Set();
+
+wss.on('connection', (ws) => {
+    clients.add(ws);
+    console.log('New WebSocket connection');
+  
+    ws.on('message', (message) => {
+      console.log('Received message:', message.toString());
+  
+      // Echo to all clients
+      for (let client of clients) {
+        if (client.readyState === ws.OPEN) {
+          client.send(message.toString().slice());
+        }
+      }
+    });
+
+    ws.on('close', () => {
+        console.log('WebSocket disconnected');
+        clients.delete(ws);
+      });
+    
+      ws.on('error', (err) => {
+        console.error('WebSocket error', err);
+      });
+    });
 
 app.listen(port, () => { console.log(`server started on port ${port}`) });
